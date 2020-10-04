9a52a061534b39bf6b5196ce7c02165573cdae6f
Ítalo Augusto <silva.morgado@hotmail.com>
2020-10-04 05:01:05 -0300

    commit

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -13,10 +13,10 @@ export default function Menu() {
 
           
 
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="#navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="collapse navbar-collapse" id="#navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
               <li class="nav-item active">
                 <Link to="/" className="Links">Home</Link>
